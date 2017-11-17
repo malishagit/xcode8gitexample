f4c711c97d6e43de1cafc7e3b08fc55d6642614a
Malisha Christian <malishachristian19@gmail.com>
2017-11-17 10:52:37 -0800

    Initial Commit

---
+++ xcode8gitexample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xcode8gitexample
+//
+//  Created by MALISHA CHRISTIAN on 11/17/17.
+//  Copyright © 2017 MALISHA CHRISTIAN. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xcode8gitexample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode8gitexample
+//
+//  Created by MALISHA CHRISTIAN on 11/17/17.
+//  Copyright © 2017 MALISHA CHRISTIAN. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
